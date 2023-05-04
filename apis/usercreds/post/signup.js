b2b946a5187eff7679ae0cb704926ee68f656798
Shubham Biswas <shubhambiswasuit.2000@gmail.com>
2023-05-04 23:23:57 +0530

    Modify the signup API

--- apis/usercreds/post/signup.js
+++ apis/usercreds/post/signup.js
@@ -9,27 +9,30 @@ const sendOtpMail = require('../../../middleware/sendMail')
 
 router.post('/signup', createToken, async (req, res) => {
     try {
-        const existingUser = User.find(req.body.email);
+        const existingUser = await User.find({ email: req.body.email });
         if (existingUser.length) {
-            console.log(existingUser);
-            return res.status(300).send({ msg: 'This email already register' })
+            req.body.otp = Math.floor(1000 + Math.random() * 9000);
+            await sendOtpMail(req.body.email, req.body.otp);
+            const user = await User.findOneAndUpdate({ email: req.body.email }, { otp: req.body.otp });
+            let userObject = user.toObject();
+            delete userObject.otp;
+            return res.status(200).send({ statue: true, user: userObject })
         }
         const isvaliduser = await validateUser(req.body)
         if (isvaliduser) {
             req.body.otp = Math.floor(1000 + Math.random() * 9000);
-            await sendOtpMail(req.body.email, req.body.otp );
+            await sendOtpMail(req.body.email, req.body.otp);
             const user = User(req.body);
-            let userObject=user.toObject();
+            let userObject = user.toObject();
             let saveUser = await user.save();
             delete userObject.otp;
-            res.status(201).send(userObject);
+            res.status(201).send({ status: true, user: userObject });
         }
     }
     catch (e) {
         console.log(e);
-        res.status(500).send({ msg: e });
+        res.status(500).send({ status: false, msg: e.message });
     }
-
 });
 
 router.post('/otp', verifyToken, async (req, res) => {
